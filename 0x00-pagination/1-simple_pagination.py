b0a2313359d57665b29ec6f38f9fe87c26889638
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-01-27 14:46:54 +0200

    Add docstring for get_page method

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -37,14 +37,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """"""
+        """Retrieves a page of data"""
         assert (type(page) == int and page > 0)
         assert (type(page_size) == int and page_size > 0)
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
-
-        if start_index >= len(dataset):
-            return []
 
         return dataset[start_index:end_index]
