c6edf665a3b46e08be1cc3e46c2a52b6b85eb958
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-01-31 18:06:21 +0200

    Add task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Defines MRUCache class"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Implement MRUCache class"""
+
+    def __init__(self):
+        super().__init__()
+        self.mru = None
+
+    def put(self, key, item):
+        """"""
+        if key is None or item is None:
+            return None
+
+        if (len(self.cache_data.keys()) >= self.MAX_ITEMS and
+                key not in self.cache_data.keys()):
+            del self.cache_data[self.mru]
+            print(f"DISCARD: {self.mru}")
+
+        self.cache_data[key] = item
+        self.mru = key
+
+    def get(self, key):
+        value = self.cache_data.get(key)
+
+        if value:
+            self.mru = key
+
+        return value
