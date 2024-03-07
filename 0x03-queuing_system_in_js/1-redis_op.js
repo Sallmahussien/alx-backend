979df20faba9a1120d603e84737c09673cd8bb01
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-03-07 10:28:24 +0200

    Remove unnecessary calls statements

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -3,22 +3,17 @@ import redis from 'redis';
 const redisClient = redis.createClient();
 
 redisClient.on('error', (error) => {
-    console.log(`Redis client not connected to the server: ${error.message}`);
+  console.log(`Redis client not connected to the server: ${error.message}`);
 });
 
 redisClient.on('connect', () => {
-    console.log('Redis client connected to the server');
+  console.log('Redis client connected to the server');
 });
 
 const setNewSchool = (schoolName, value) => {
-    redisClient.set(schoolName, value, redis.print);
+  redisClient.set(schoolName, value, redis.print);
 }
 
 const displaySchoolValue = (schoolName) => {
-    redisClient.get(schoolName, redis.print);
+  redisClient.get(schoolName, redis.print);
 }
-
-displaySchoolValue('Holberton');
-setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
-
